390b5b820e7580be61dde799d04adf11c30a70e2
Victowolf <hosalliyash@gmail.com>
2026-08-30 11:56:10 +0530

    start update

--- start.sh
+++ start.sh
@@ -11,19 +11,21 @@ echo "[1] Python version"
 python --version
 
 echo
-echo "[2] GPU"
+echo "[2] Existing PyTorch"
+python -c "import torch; print('Torch:', torch.__version__); print('CUDA:', torch.cuda.is_available())"
+
+echo
+echo "[3] GPU"
 nvidia-smi
 
 echo
-echo "[3] Installing requirements"
-
-python -m pip install --upgrade pip
+echo "[4] Installing dependencies"
 
 python -m pip install \
     --no-cache-dir \
     -r requirements.txt
 
 echo
-echo "[4] Starting main.py"
+echo "[5] Starting main.py"
 
 exec python main.py
